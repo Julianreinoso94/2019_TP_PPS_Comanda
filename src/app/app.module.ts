a769161cd0b32dcc67b79e8c42fc14d2f127ed9d
julianreinoso94 <julianreinoso94@gmail.com>
2019-05-29 00:24:16 -0300

    Generando Alta duenio, con los servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,20 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { Camera } from '@ionic-native/camera/ngx';
 import { BarcodeScanner } from "@ionic-native/barcode-scanner/ngx";
+import { environment } from '../environments/environment';
+//import { AngularFireModule } from 'angularfire2';
+
+import { AngularFireModule } from '@angular/fire';
+
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule,AngularFireModule.initializeApp(environment.firebase),
+AngularFirestoreModule,
+    AngularFireDatabaseModule,
+   IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
   providers: [
     StatusBar,
     SplashScreen,BarcodeScanner,
