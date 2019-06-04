46e776a2540b0b7d9d0e6342f9ee3d14ea8c0a89
Santiago de la Fuente <santiago.daniel.delafuente@hotmail.com>
2019-06-04 19:44:11 -0300

    ABM comidas2.0, sin fotos

--- src/app/pages/comida-detail/comida-detail.page.ts
+++ src/app/pages/comida-detail/comida-detail.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ComidasService } from '../../services/comidas/comidas.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-comida-detail',
   templateUrl: './comida-detail.page.html',
   styleUrls: ['./comida-detail.page.scss'],
 })
 export class ComidaDetailPage implements OnInit {
-
-  constructor() { }
+  public comidaActual: any = {};
+  constructor(
+    private comidaService: ComidasService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit() {
+    const comidaId: string = this.route.snapshot.paramMap.get('id');
+    this.comidaService
+      .getDetalleComida(comidaId)
+      .get()
+      .then(eventSnapshot => {
+        this.comidaActual = eventSnapshot.data();
+        this.comidaActual.id = eventSnapshot.id;
+      });
   }
 
 }
