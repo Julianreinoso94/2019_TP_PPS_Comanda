ae9c0bc03ff506a0aa5a8cb7beb161138e7605f0
julianreinoso94 <julianreinoso94@gmail.com>
2019-06-05 22:06:18 -0300

    guardando camnbios qrmesa

---
+++ src/app/pages/qrmesa/qrmesa.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { QrmesaPage } from './qrmesa.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: QrmesaPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [QrmesaPage]
+})
+export class QrmesaPageModule {}
