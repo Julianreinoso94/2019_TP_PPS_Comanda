50ab31aa5483d0bde325c028ae68ed533ba534fd
julianreinoso94 <julianreinoso94@gmail.com>
2019-06-09 19:57:20 -0300

    Update app.component.ts
    Pull master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FCM } from '@ionic-native/fcm/ngx';
 
 import * as firebase from 'firebase/app';
 import { environment } from '../environments/environment';
@@ -14,7 +15,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,  private fcm: FCM
+
   ) {
     this.initializeApp();
   }
@@ -24,6 +26,25 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.fcm.getToken().then(token => {
+  console.log(token);
+});
+
+this.fcm.onTokenRefresh().subscribe(token => {
+  console.log(token);
+});
+
+this.fcm.onNotification().subscribe(data => {
+  console.log(data);
+  if (data.wasTapped) {
+    console.log('Received in background');
+    this.router.navigate([data.landing_page, data.price]);
+  } else {
+    console.log('Received in foreground');
+    this.router.navigate([data.landing_page, data.price]);
+  }
+});
     });
   }
 }
