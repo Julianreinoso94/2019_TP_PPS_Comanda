89623c08d3a4da82b898b3c898772acfaa0d82e0
julianreinoso94 <julianreinoso94@gmail.com>
2019-06-09 11:56:42 -0300

    Agregando Roles

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProfileService } from './../services/user/profile.service';
+import {AuthService} from "./../services/user/auth.service";
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+    public userProfile: any;
+    public birthDate: Date;
+    public perfil:string;
+    public valor="hola";
+
+
+    constructor(
+      private alertCtrl: AlertController,
+      private authService: AuthService,
+      private profileService: ProfileService,
+
+    ) { }
+  ngOnInit() {
+    this.profileService
+      .getUserProfile()
+      .get()
+      .then( userProfileSnapshot => {
+        this.userProfile = userProfileSnapshot.data();
+        console.log(this.userProfile);
+        this.birthDate = userProfileSnapshot.data().birthDate;
+        this.perfil= userProfileSnapshot.data().perfil;
+      });
+    //  console.log(this.userProfile.perfil);
+
+  }
 
 }
