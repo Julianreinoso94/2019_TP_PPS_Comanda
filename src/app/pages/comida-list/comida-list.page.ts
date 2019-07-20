3ebc6cbeebcc3bb7e3cf27352fe9fbffa87d963d
Eveyazmins <eveyazmins@gmail.com>
2019-07-19 21:00:59 -0300

    Listado de comidas funcionando OK.
    se modifica detalle, listado

--- src/app/pages/comida-list/comida-list.page.ts
+++ src/app/pages/comida-list/comida-list.page.ts
@@ -10,6 +10,9 @@ export class ComidaListPage implements OnInit {
 
   public comidasList: Array<any>;
   public userProfile: any;
+  public comidas: any;
+  public bebidas: any;
+  public postres: any;
 
   constructor(
     private comidasService: ComidasService,
@@ -29,25 +32,82 @@ export class ComidaListPage implements OnInit {
             description: snap.data().description,
             price: snap.data().price,
             time: snap.data().time,
+            tipo: snap.data().tipo
           });
           // return false;
         });
       });
 
+    
     this.profileService
       .getUserProfile()
       .get()
       .then( userProfileSnapshot => {
         this.userProfile = userProfileSnapshot.data();
       });
+
+
+      this.comidasService.traerComidas().subscribe(data => {
+      
+        this.comidas = data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            isEdit: false,
+            codigo: e.payload.doc.data()['comidaCodigo'],
+            nombre: e.payload.doc.data()['name'],
+            tipo: e.payload.doc.data()['tipo'],
+            precio: e.payload.doc.data()['price'],
+            tiempo: e.payload.doc.data()['time'],
+          };
+        })
+        console.log(this.comidas);
+      });
+
+      
+      this.comidasService.traerBebidas().subscribe(data => {
+        this.bebidas = data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            isEdit: false,
+            codigo: e.payload.doc.data()['comidaCodigo'],
+            nombre: e.payload.doc.data()['name'],
+            tipo: e.payload.doc.data()['tipo'],
+            precio: e.payload.doc.data()['price'],
+            tiempo: e.payload.doc.data()['time'],
+          };
+        })
+        console.log(this.bebidas);
+      });
+
+      this.comidasService.traerPostres().subscribe(data => {
+        this.postres = data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            isEdit: false,
+            codigo: e.payload.doc.data()['comidaCodigo'],
+            nombre: e.payload.doc.data()['name'],
+            tipo: e.payload.doc.data()['tipo'],
+            precio: e.payload.doc.data()['price'],
+            tiempo: e.payload.doc.data()['time'],
+          };
+        })
+        console.log(this.postres);
+      });
+      
   }
 
   ionViewWillEnter() {
     this.ngOnInit();
   }
+
   borrarComida(id) {
     this.comidasService.deleteComida(id);
     this.ngOnInit();
     // this.navCtrl.setRoot(this.navCtrl.getActive().component);
   }
+
+ 
+
+
+
 }
